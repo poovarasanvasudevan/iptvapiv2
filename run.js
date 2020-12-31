345e2269f5a1bff9a2986e2dff68a24c80a813cc
poovarasan <poosan9@gmail.com>
2020-12-31 10:39:14 +0530

    Changes in Thursday 31 December 2020 10:39:12 AM IST

--- run.js
+++ run.js
@@ -27,7 +27,7 @@ http.get(URL, (res) => {
 
 
 function createData(json) {
-    console.log(json)
+
     fs.writeFileSync(path.join(__dirname, 'api', 'channel.json'), JSON.stringify(json));
 
     const category = []
@@ -39,59 +39,62 @@ function createData(json) {
     const globalCountry = {}
     const globalLanguage = {}
     json.forEach((item) => {
-        if (item.category === null) {
-            if (globalCategory.hasOwnProperty("nocategory")) {
-                globalCategory['nocategory'].push(item)
-            } else {
-                globalCategory['nocategory'] = []
-                globalCategory['nocategory'].push(item)
-            }
-        } else {
-            if (globalCategory.hasOwnProperty(item.category)) {
-                globalCategory[item.category].push(item)
-            } else {
-                globalCategory[item.category] = []
-                globalCategory[item.category].push(item)
-            }
-        }
-
-
-        if (item.country == null) {
-            if (globalCountry.hasOwnProperty("nocountry")) {
-                globalCountry['nocountry'].push(item)
-            } else {
-                globalCountry['nocountry'] = []
-                globalCountry['nocountry'].push(item)
-            }
-        } else {
-            if (globalCountry.hasOwnProperty(item.country.code + "!" + item.country.name)) {
-                globalCountry[item.country.code + "!" + item.country.name].push(item)
-            } else {
-                globalCountry[item.country.code + "!" + item.country.name] = []
-                globalCountry[item.country.code + "!" + item.country.name].push(item)
-            }
-        }
-
-
-        if (item.language == null || item.language.length === 0) {
-            if (globalLanguage.hasOwnProperty("nolanguage")) {
-                globalLanguage['nolanguage'].push(item)
-            } else {
-                globalLanguage['nolanguage'] = []
-                globalLanguage['nolanguage'].push(item)
-            }
-        } else {
-
-            item.language.forEach((lang) => {
-                if (globalLanguage.hasOwnProperty(lang.code + "!" + lang.name)) {
-                    globalLanguage[lang.code + "!" + lang.name].push(item)
+        if(item.category !== 'XXX') {
+            if (item.category === null) {
+                if (globalCategory.hasOwnProperty("nocategory")) {
+                    globalCategory['nocategory'].push(item)
                 } else {
-                    globalLanguage[lang.code + "!" + lang.name] = []
-                    globalLanguage[lang.code + "!" + lang.name].push(item)
+                    globalCategory['nocategory'] = []
+                    globalCategory['nocategory'].push(item)
                 }
-            })
+            } else {
+                if (globalCategory.hasOwnProperty(item.category)) {
+                    globalCategory[item.category].push(item)
+                } else {
+                    globalCategory[item.category] = []
+                    globalCategory[item.category].push(item)
+                }
+            }
+
+
+            if (item.country == null) {
+                if (globalCountry.hasOwnProperty("nocountry")) {
+                    globalCountry['nocountry'].push(item)
+                } else {
+                    globalCountry['nocountry'] = []
+                    globalCountry['nocountry'].push(item)
+                }
+            } else {
+                if (globalCountry.hasOwnProperty(item.country.code + "!" + item.country.name)) {
+                    globalCountry[item.country.code + "!" + item.country.name].push(item)
+                } else {
+                    globalCountry[item.country.code + "!" + item.country.name] = []
+                    globalCountry[item.country.code + "!" + item.country.name].push(item)
+                }
+            }
+
+
+            if (item.language == null || item.language.length === 0) {
+                if (globalLanguage.hasOwnProperty("nolanguage")) {
+                    globalLanguage['nolanguage'].push(item)
+                } else {
+                    globalLanguage['nolanguage'] = []
+                    globalLanguage['nolanguage'].push(item)
+                }
+            } else {
+
+                item.language.forEach((lang) => {
+                    if (globalLanguage.hasOwnProperty(lang.code + "!" + lang.name)) {
+                        globalLanguage[lang.code + "!" + lang.name].push(item)
+                    } else {
+                        globalLanguage[lang.code + "!" + lang.name] = []
+                        globalLanguage[lang.code + "!" + lang.name].push(item)
+                    }
+                })
+            }
         }
     })
+
 
 
     Object.keys(globalCategory).map(key => {
